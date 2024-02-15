2529ffb4f00bf21da38e7e4d9f471b1417515f92
annapatrakeeva <129195218+annapatrakeeva@users.noreply.github.com>
2024-02-15 13:44:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,119 @@
 import openmc
 import openmc.deplete
-import sys
+
 import openmc.model
 from openmc import stats
+from math import sqrt
 import neutronics_material_maker as nmm
-from mat import water_mat,  AbstractUO2, Gd2O3_mat, helium_mat, E110_mat, E635_mat, steel_mat, Dy2O3TiO2_mat, B4C_mat, AbstractUO2
-#openmc.config['cross_sections']='/home/ann/PycharmProjects/1000/library/jeff33 (1)/jeff-3.3-hdf5/cross_sections.xml'
-openmc.config['cross_sections']='/home/ann/PycharmProjects/library_/endfb71/endfb-vii.1-hdf5/cross_sections.xml'
+from mat import water_mat,  AbstractUO2, Gd2O3_mat, helium_mat, E110_mat, E635_mat, steel_mat,  B4C_mat
+
+openmc.config['cross_sections']='/home/ann/PycharmProjects/endfb-vii.1-hdf5/cross_sections.xml'
 
 
 import matplotlib.pyplot as plt
-from core import get_TVS_universe
+from core import get_TVS_universe, basket_universe, top_surf, bottom_surf
 from params import GeometryParams
 
 
 import numpy as np
 if __name__ == "__main__":
-    top_surf=openmc.ZPlane(z0=186.5)
-    bottom_surf=openmc.ZPlane(z0=-186.5)
-    top_surf.boundary_type='reflective'
-    bottom_surf.boundary_type='reflective'
+    #top_surf=openmc.ZPlane(z0=177.5)
+    #bottom_surf=openmc.ZPlane(z0=-177.5)
+    #top_surf.boundary_type='reflective'
+    #bottom_surf.boundary_type='reflective'
+
 
     mats=[]
     mats_id=[]
-    TVS1, mats1=get_TVS_universe(3.0, 2.4 , False, 1, 0,1.6, 0)
-    mats+=mats1
+    TVS_30AV5, mats_30AV5=get_TVS_universe(3.0, 2.4, False, 1, 0,False, 0, 0)
+    mats+=mats_30AV5
     mats_id=[mat.id for mat in mats]
-    TVS2, mats2 =get_TVS_universe(1.3, 0, False, 0, 0, 0,  verbose=False)
-    for mat in mats2:
+    TVS_13AU, mats_13AU =get_TVS_universe(1.3, 0, False, 0, False, False,  0, 0)
+    for mat in mats_13AU:
         if mat.id not in mats_id:
             mats.append(mat)
             mats_id.append(mat.id)
-    materials = openmc.Materials([ Gd2O3_mat, helium_mat, E110_mat, E635_mat, steel_mat, Dy2O3TiO2_mat, B4C_mat]+mats)
 
+    TVS_22AU, mats_22AU=get_TVS_universe(2.2, 0, False, 0, False, False, False, 0)
+    for mat in mats_22AU:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_39AWU, mats_39AWU=get_TVS_universe(4.0, 3.3, False, 1, 0, 1, 3.6, 0)
+    for mat in mats_39AWU:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_390GO, mats_390GO=get_TVS_universe(4.0, 3.3, False, 1, 1, 1, 3.6, 0)
+    for mat in mats_390GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_398GO, mats_398GO=get_TVS_universe(4.4, 3.3, False, 1, 1, 0, False, 0)
+    for mat in mats_398GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_430GO, mats_430GO=get_TVS_universe(4.4, 3.6, False, 1, 1, 1, 4.0, 0)
+    for mat in mats_430GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_439GO, mats_439GO=get_TVS_universe(4.4, 3.6, False, 1, 0, 0, False, 0)
+    for mat in mats_439GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    materials = openmc.Materials([ Gd2O3_mat, helium_mat, E110_mat, E635_mat, steel_mat,  B4C_mat]+mats)
     all_water2_cell = openmc.Cell(fill=water_mat)
-    water2_universe = openmc.Universe(cells=[all_water2_cell, ])
+    water2_universe = openmc.Universe(cells=(all_water2_cell, ))
     core_lat = openmc.HexLattice()
     core_lat.center = (0.0, 0.0)
     core_lat.pitch = [23.6]
     core_lat.outer = water2_universe
-    core_lat.orientation = 'y'
-    outer_ring = [TVS1] * 6
-    inner2_ring = [TVS2]
-    core_lat.universes = [outer_ring, inner2_ring]
-    outer2_radius = 100
-    outer2_surf = openmc.ZCylinder(r=outer2_radius, boundary_type='reflective')
+    core_lat.orientation = 'x'
+    ring_1=[basket_universe]+[TVS_390GO]+[TVS_39AWU]*4+ [TVS_390GO] #42
+    ring_1*=6
+    ring_2=[TVS_30AV5] + [TVS_22AU] #36
+    ring_2*=18
+    ring_3=[TVS_22AU]+[TVS_13AU]*4 #30
+    ring_3*=6
+    ring_4=[TVS_13AU] + [TVS_30AV5] + [TVS_22AU] +[TVS_30AV5] #24
+    ring_4*=6
+    ring_5= [TVS_22AU] + [TVS_13AU]*2 #18
+    ring_5*=6
+    ring_6= [TVS_30AV5] + [TVS_22AU] #12
+    ring_6*=6
+    ring_7=[TVS_13AU]*6
+    ring_8=[TVS_30AV5]
+    core_lat.universes = [ring_1, ring_2, ring_3, ring_4, ring_5, ring_6, ring_7, ring_8]
+
+    outer2_surf = openmc.ZCylinder(r=178, boundary_type='vacuum')
     core_cell = openmc.Cell(fill=core_lat, region=-outer2_surf & +bottom_surf & -top_surf)
 
     materials.export_to_xml()
     params = GeometryParams()
-    # print(universe.get_all_materials())
     geometry=openmc.Geometry()
-    geometry.root_universe=openmc.Universe(0, cells=[core_cell,])
+    geometry.root_universe=openmc.Universe( universe_id=0, cells=[core_cell,])
     settings=openmc.Settings()
     settings.temperature={'method': 'interpolation'}
-    uniform_dist = stats.Box([-10, -10, -350 / 2], [10, 10, 350 / 2], only_fissionable=True)
+    uniform_dist = stats.Box([-178, -178, -355 / 2], [178, 178, 355 / 2], only_fissionable=True)
     source = openmc.source.Source(space=uniform_dist)
     source.time = stats.Uniform(0, 1)
     settings.source = source
-    flux_tally = openmc.Tally(name='flux')
-    flux_tally.scores = ['flux']
-    U_tally = openmc.Tally(name='fuel')
-    U_tally.scores = ['fission', 'total', 'absorption', 'elastic', 'scatter', 'decay-rate']
-    U_tally.nuclides = ['U235', 'U238', 'O16', 'H1']
+    tallies_file = openmc.Tallies()
+    fiss_rate = openmc.Tally(name='fiss. rate')
+    abs_rate = openmc.Tally(name='abs. rate')
+    n_rate = openmc.Tally(name='n_rate')
+
+    fiss_rate.scores = ['nu-fission']
+    abs_rate.scores = ['absorption']
+    n_rate.scores = ['(n,2n)']
+    tallies_file += (fiss_rate, abs_rate, n_rate)
+    tallies_file.export_to_xml()
     settings.batches = 100
-    settings.particles = 6000
-    settings.inactive = 10
+    settings.particles = 80000
+    settings.inactive = 20
     #power = (3000.0e6)/163  # watts
     #model = openmc.Model(geometry, materials, settings)
     #chain_file = '/home/ann/PycharmProjects/1000/library/chain_endfb71_pwr.xml'
@@ -85,27 +133,26 @@ if __name__ == "__main__":
 
 
 
-    #plots = openmc.Plots(plots)
+
     colors = {water_mat: (32, 178, 170)}
     color_data = dict(color_by='material', colors=colors)
-    width = np.array([params.TVS_edge_length * 5.1, params.TVS_edge_length * 5.1, ])
-    scale = 5.1 / 2
+    width = np.array([24.6 *16, 24.6 * 16, ])
+    scale = 1
     fig, ax = plt.subplots(2, 2)
 
-    geometry.root_universe.plot(width=width / scale, pixels=(1000, 1000), basis='xz', **color_data,
+    geometry.root_universe.plot(width=width / scale, pixels=(100, 100), basis='xz', **color_data,
                   origin=(0, 0, GeometryParams.tvel_heigh / 2 - 1), axes=ax[0][0])
-    geometry.root_universe.plot(width=width / scale, pixels=(1000, 1000), basis='xz', **color_data, origin=(0, 0, 0), axes=ax[1][1])
-    geometry.root_universe.plot(width=width / scale, pixels=(1000, 1000), basis='xz', **color_data,
+    geometry.root_universe.plot(width=width / scale, pixels=(100, 100), basis='xz', **color_data, origin=(0, 0, 0), axes=ax[1][1])
+    geometry.root_universe.plot(width=width / scale, pixels=(100, 100), basis='xz', **color_data,
                   origin=(0, 0, -GeometryParams.tvel_heigh / 2 + 1),
                   axes=ax[0][1])
-    geometry.root_universe.plot(width=width / scale, pixels=(1000, 1000), basis='xy', **color_data, origin=(0, 0, 0), axes=ax[1][0])
-    plt.savefig('plots/geometry.jpg')
+    geometry.root_universe.plot(width=width / scale, pixels=(10000, 10000), basis='xy', **color_data, origin=(0, 0, 0), axes=ax[1][0])
 
     # ...and by openmc.Plots
     plots = [openmc.Plot(), openmc.Plot(), openmc.Plot(), openmc.Plot(), ]
     for i in range(4):
         plots[i].width = width
-        plots[i].pixels = (2000, 2000)
+        plots[i].pixels = (6000, 6000)
         plots[i].basis = 'xz'
         plots[i].color_by = 'material'
         plots[i].colors = colors
@@ -115,13 +162,12 @@ if __name__ == "__main__":
 
     plots = openmc.Plots(plots)
 
-    tallies_file=openmc.Tallies([flux_tally, U_tally])
-    tallies_file.export_to_xml('xmlki/tallies.xml')
 
 
-    plots.export_to_xml('xmlki/plots.xml')
-    settings.export_to_xml('xmlki/settings.xml')
-    geometry.export_to_xml('geometry.xml')
+
+    plots.export_to_xml()
+    settings.export_to_xml()
+    geometry.export_to_xml()
     #model.export_to_xml('model.xml')
     openmc.plot_geometry()
-    openmc.run()
+    #openmc.run()
