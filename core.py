f241131b7060f17120ec9fbe8164b5c1a354d2f2
annapatrakeeva <129195218+annapatrakeeva@users.noreply.github.com>
2024-04-10 11:24:21 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,155 +1,186 @@
 import openmc
-from mat import  water_mat,  AbstractUO2, Gd2O3_mat, helium_mat, E110_mat, E635_mat, steel_mat, Dy2O3TiO2_mat, B4C_mat, basket_mat, water_mat1
+import openmc.deplete
+
+import openmc.model
+from openmc import stats
 from math import sqrt
-from config import *
-import random
-top_surf=openmc.ZPlane(z0=177.5)
-bottom_surf=openmc.ZPlane(z0=-177.5)
-top_surf.boundary_type = 'reflective'
-bottom_surf.boundary_type = 'reflective'
-water_b_surf = openmc.model.HexagonalPrism(edge_length=23.6 / sqrt(3), orientation='y', boundary_type='transmission')
-basket_cell = openmc.Cell(fill=basket_mat, region=-water_b_surf & +bottom_surf & -top_surf)
-basket_universe = openmc.Universe(cells=[basket_cell])
+import neutronics_material_maker as nmm
+from mat import water_mat,  AbstractUO2, Gd2O3_mat, helium_mat, E110_mat, E635_mat, steel_mat,  B4C_mat, basket_mat, water_mat1, be_mat
 
-def get_TVS_universe(enr, enr_tvegs, rods_inserted=False, tvegs=False, tvegs_5inner_ring=False,  outer_ring=False, outer_ring_enr=False, verbose=True):
-
-     #if outer_ring and not outer_ring_enr:
-         #raise Exception('Outer ring enrichment must be set')
+openmc.config['cross_sections']='/home/ann/PycharmProjects/endfb-vii.1-hdf5/cross_sections.xml'
 
 
-
-    UO2_mat1 = AbstractUO2(enr).mat
-    UO2_mat2=AbstractUO2(enr_tvegs).mat
-
-    if tvegs:
-        UO2_mat2 = openmc.Material.mix_materials(
-   materials=[
-        UO2_mat2,
-        Gd2O3_mat,
-    ],
-    fracs=[0.95, 0.05],
-    percent_type='vo')
-
-    else:
-        UO2_mat2 = UO2_mat1
-    if outer_ring:
-        UO2_mat3 = AbstractUO2(outer_ring_enr).mat
-    else:
-        UO2_mat3=UO2_mat1
-
-    tvel_helium_surf=openmc.ZCylinder(r=r1_tvel)
-    tvel_fuel_surf=openmc.ZCylinder(r=r2_tvel)
-    tvel_helium2_surf=openmc.ZCylinder(r=r3_tvel)
-    tvel_cladding_surf=openmc.ZCylinder(r=r4_tvel)
-
-    central_surf1=openmc.ZCylinder(r=r1_central)
-    central_surf2=openmc.ZCylinder(r=r2_central)
-
-    cyz_absorber_surf=openmc.ZCylinder(r=r1_guide)
-    cyz_steel_surf=openmc.ZCylinder(r=r2_guide)
-    cyz_coolant_surf=openmc.ZCylinder(r=r3_guide)
-    cyz_cladding_surf=openmc.ZCylinder(r=r4_guide)
-
-    tvel_helium_lower_surf=openmc.ZCylinder(r=r1_tvel_low)
-    tvel_cladding_lower_surf=openmc.ZCylinder(r=r2_tvel_low)
-
-    tvel_helium_upper_surf=openmc.ZCylinder(r=r1_tvel_upper)
-    tvel_cladding_upper_surf=openmc.ZCylinder(r=r2_tvel_upper)
-
-    water_surf=openmc.model.HexagonalPrism(edge_length=1.275/sqrt(3), orientation='x', boundary_type='transmission')
+import matplotlib.pyplot as plt
+from core import get_TVS_universe, basket_universe
+from params import GeometryParams
 
 
-
-    tvel1_helium1_cell=openmc.Cell(fill=helium_mat, region=-tvel_helium_surf & +bottom_surf & -top_surf)
-    tvel1_fuel_cell=openmc.Cell(fill=UO2_mat1, region=+tvel_helium_surf & -tvel_fuel_surf & +bottom_surf & -top_surf)
-    tvel1_helium2_cell=openmc.Cell(fill=helium_mat, region=+tvel_fuel_surf & -tvel_helium2_surf  & +bottom_surf & -top_surf)
-    tvel1_cladding_cell=openmc.Cell(fill=E110_mat, region=+tvel_helium2_surf & -tvel_cladding_surf & +bottom_surf & -top_surf)
-    tvel1_water_cell=openmc.Cell(fill=water_mat, region=+tvel_cladding_surf & -water_surf & +bottom_surf & -top_surf)
-
-    tvel2_helium1_cell=openmc.Cell(fill=helium_mat, region=-tvel_helium_surf & +bottom_surf & -top_surf)
-    tvel2_fuel_cell=openmc.Cell(fill=UO2_mat2, region=+tvel_helium_surf & -tvel_fuel_surf & +bottom_surf & -top_surf)
-    tvel2_helium2_cell=openmc.Cell(fill=helium_mat, region=+tvel_fuel_surf & -tvel_helium2_surf  & +bottom_surf & -top_surf)
-    tvel2_cladding_cell=openmc.Cell(fill=E110_mat, region=+tvel_helium2_surf & -tvel_cladding_surf & +bottom_surf & -top_surf)
-    tvel2_water_cell=openmc.Cell(fill=water_mat, region=+tvel_cladding_surf & -water_surf & +bottom_surf & -top_surf)
-
-    tvel3_helium1_cell=openmc.Cell(fill=helium_mat, region=-tvel_helium_surf & +bottom_surf & -top_surf)
-    tvel3_fuel_cell=openmc.Cell(fill=UO2_mat3, region=+tvel_helium_surf & -tvel_fuel_surf & +bottom_surf & -top_surf)
-    tvel3_helium2_cell=openmc.Cell(fill=helium_mat, region=+tvel_fuel_surf & -tvel_helium2_surf  & +bottom_surf & -top_surf)
-    tvel3_cladding_cell=openmc.Cell(fill=E110_mat, region=+tvel_helium2_surf & -tvel_cladding_surf & +bottom_surf & -top_surf)
-    tvel3_water_cell=openmc.Cell(fill=water_mat, region=+tvel_cladding_surf & -water_surf & +bottom_surf & -top_surf)
-
-    #tvel4_helium1_cell = openmc.Cell(fill=helium_mat, region=-tvel_helium_surf & +bottom_surf & -top_surf)
-   # tvel4_fuel_cell = openmc.Cell(fill=UO2_mat4, region=+tvel_helium_surf & -tvel_fuel_surf & +bottom_surf & -top_surf)
-    #tvel4_helium2_cell = openmc.Cell(fill=helium_mat,
-           #                           region=+tvel_fuel_surf & -tvel_helium2_surf & +bottom_surf & -top_surf)
-   # tvel4_cladding_cell = openmc.Cell(fill=E110_mat,
-               #                        region=+tvel_helium2_surf & -tvel_cladding_surf & +bottom_surf & -top_surf)
-   # tvel4_water_cell = openmc.Cell(fill=water_mat, region=+tvel_cladding_surf & water_surf & +bottom_surf & -top_surf)
-
-    tvel1_universe=openmc.Universe(cells=[tvel1_helium1_cell, tvel1_fuel_cell, tvel1_helium2_cell, tvel1_cladding_cell, tvel1_water_cell])
-    tvel2_universe=openmc.Universe(cells=[tvel2_helium1_cell, tvel2_fuel_cell, tvel2_helium2_cell, tvel2_cladding_cell, tvel2_water_cell])
-    tvel3_universe=openmc.Universe(cells=[tvel3_helium1_cell, tvel3_fuel_cell, tvel3_helium2_cell, tvel3_cladding_cell, tvel3_water_cell])
-    #tvel4_universe = openmc.Universe(cells=[tvel4_helium1_cell, tvel4_fuel_cell, tvel4_helium2_cell, tvel4_cladding_cell, tvel4_water_cell])
+import numpy as np
+if __name__ == "__main__":
+    top_surf=openmc.ZPlane(z0=177.5)
+    bottom_surf=openmc.ZPlane(z0=-177.5)
+    top_surf.boundary_type='reflective'
+    bottom_surf.boundary_type='reflective'
 
 
-    if rods_inserted:
-        guide_mat = B4C_mat
-    else:
-        guide_mat = water_mat
+    mats=[]
+    mats_id=[]
+    TVS_30AV5, mats_30AV5=get_TVS_universe(3.0, 2.4, False, 1, 0,False, 0, 0)
+    mats+=mats_30AV5
+    mats_id=[mat.id for mat in mats]
+    TVS_13AU, mats_13AU =get_TVS_universe(1.3, 0, False, 0, False, False,  0, 0)
+    for mat in mats_13AU:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
 
-    guide_absorber_cell=openmc.Cell(fill=guide_mat, region=-cyz_absorber_surf & +bottom_surf & -top_surf)
-    guide_steel_cell=openmc.Cell(fill=steel_mat, region=-cyz_steel_surf & +cyz_absorber_surf & +bottom_surf & -top_surf)
-    guide_water1_cell=openmc.Cell(fill=water_mat, region=-cyz_coolant_surf & +cyz_steel_surf & +bottom_surf & -top_surf)
-    guide_cladding_cell=openmc.Cell(fill=E635_mat, region=-cyz_cladding_surf & +cyz_coolant_surf & +bottom_surf & -top_surf )
-    guide_water2_cell=openmc.Cell(fill=water_mat, region=+cyz_cladding_surf & -water_surf & +bottom_surf & -top_surf)
+    TVS_22AU, mats_22AU=get_TVS_universe(2.2, 0, False, 0, False, False, False, 0)
+    for mat in mats_22AU:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_39AWU, mats_39AWU=get_TVS_universe(4.0, 3.3, False, 1, 0, 1, 3.6, 0)
+    for mat in mats_39AWU:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_390GO, mats_390GO=get_TVS_universe(4.0, 3.3, False, 1, 1, 1, 3.6, 0)
+    for mat in mats_390GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_398GO, mats_398GO=get_TVS_universe(4.4, 3.3, False, 1, 1, 0, False, 0)
+    for mat in mats_398GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_430GO, mats_430GO=get_TVS_universe(4.4, 3.6, False, 1, 1, 1, 4.0, 0)
+    for mat in mats_430GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    TVS_439GO, mats_439GO=get_TVS_universe(4.4, 3.6, False, 1, 0, 0, False, 0)
+    for mat in mats_439GO:
+        if mat.id not in mats_id:
+            mats.append(mat)
+            mats_id.append(mat.id)
+    materials = openmc.Materials([ Gd2O3_mat, helium_mat, E110_mat, E635_mat, steel_mat,  B4C_mat, basket_mat, water_mat1, be_mat]+mats)
+    all_water2_cell = openmc.Cell(fill=basket_mat)
+    water2_universe = openmc.Universe(cells=(all_water2_cell, ))
+    core_lat = openmc.HexLattice()
+    core_lat.center = (0.0, 0.0)
+    core_lat.pitch = [11*1.275*sqrt(3)]
+    core_lat.outer = water2_universe
+    core_lat.orientation = 'x'
+    ring_1=[basket_universe]+[TVS_390GO]+[TVS_39AWU]*4+ [TVS_390GO] #42
+    ring_1*=6
+    ring_2=[TVS_30AV5] + [TVS_22AU] #36
+    ring_2*=18
+    ring_3=[TVS_22AU]+[TVS_13AU]*4 #30
+    ring_3*=6
+    ring_4=[TVS_13AU] + [TVS_30AV5] + [TVS_22AU] +[TVS_30AV5] #24
+    ring_4*=6
+    ring_5= [TVS_22AU] + [TVS_13AU]+[TVS_13AU] #18
+    ring_5*=6
+    ring_6= [TVS_30AV5] + [TVS_22AU] #12
+    ring_6*=6
+    ring_7=[TVS_13AU]*6
+    ring_8=[TVS_30AV5]
+    core_lat.universes = [ring_1,  ring_2, ring_3, ring_4, ring_5, ring_6, ring_7, ring_8]
 
-    guide_universe=openmc.Universe(cells=[guide_absorber_cell, guide_steel_cell, guide_water1_cell, guide_water1_cell, guide_cladding_cell, guide_water2_cell] )
-    print('ghghhgg', guide_mat )
-    central_cell1=openmc.Cell(fill=water_mat, region=-central_surf1 & +bottom_surf & -top_surf)
-    central_cell2=openmc.Cell(fill=E635_mat, region=+central_surf1 & -central_surf2 & +bottom_surf & -top_surf)
-    central_water_cell=openmc.Cell(fill=water_mat, region= +central_surf2 & -water_surf & +bottom_surf & -top_surf)
+    outer2_surf = openmc.model.HexagonalPrism(edge_length=8*core_lat.pitch[0], orientation='x', boundary_type='white')
+    core_cell = openmc.Cell(fill=core_lat, region=-outer2_surf & +bottom_surf & -top_surf)
+    materials.export_to_xml()
+    params = GeometryParams()
+    geometry=openmc.Geometry()
+    geometry.root_universe=openmc.Universe( universe_id=0, cells=[core_cell,])
+    settings=openmc.Settings()
+    settings.temperature={'method': 'interpolation'}
+    uniform_dist = stats.Box([-90, -90, -355 / 2], [90, 90, 355 / 2], only_fissionable=True)
+    source = openmc.source.Source(space=uniform_dist)
+    source.time = stats.Uniform(0, 1)
+    settings.source = source
+    tallies_file = openmc.Tallies()
+    fiss_rate = openmc.Tally(name='fiss. rate')
+    abs_rate = openmc.Tally(name='abs. rate')
+    n_rate = openmc.Tally(name='n_rate')
 
-    central_universe=openmc.Universe(cells=[central_cell1, central_cell2, central_water_cell],)
+    fiss_rate.scores = ['nu-fission']
+    abs_rate.scores = ['absorption']
+    n_rate.scores = ['(n,2n)']
+    tallies_file += (fiss_rate, abs_rate, n_rate)
+    tallies_file.export_to_xml()
+    settings.batches = 100
+    settings.particles = 80000
+    settings.inactive = 10
+    #settings.trace=(1, 1, 25722)
+    #settings.max_tracks = 1000
+    #settings.tracks=[
+    #    (1, 1, 28479),
+    #    (1, 1, 24983)
+    #]
+    #trace2=settings.trace=(1, 1, 24983)
+    #trace3=settings.trace=(1, 1, 20593)
+    #settings_file+=(trace1, trace2, trace3)
+    #trace_file.export_to_xml()
+    settings.max_lost_particles=2000
+    #settings.max_write_lost_particles
+    #power = (3000.0e6)/163  # watts
+    model = openmc.Model(geometry, materials, settings)
+    #chain_file = '/home/ann/PycharmProjects/1000/library/chain_endfb71_pwr.xml'
+    #operator = openmc.deplete.CoupledOperator(model, chain_file)
+    #timesteps = [ (2,'MWd/kg'), (4,'MWd/kg'), (6,'MWd/kg'), (8,'MWd/kg'), (10,'MWd/kg'), (12,'MWd/kg'), (14,'MWd/kg'), (15,'MWd/kg'), (20,'MWd/kg'), (40, 'MWd/kg')]  # days
+    #integrator=openmc.deplete.PredictorIntegrator(operator, timesteps,  power_density=108.0e6, timestep_units='MWd/kg')
 
-    all_water_cell=openmc.Cell(fill=water_mat)
-    water_universe=openmc.Universe(cells=[all_water_cell,])
 
-    lat = openmc.HexLattice()
-    lat.center = (0.0, 0.0)
-    lat.pitch = [1.275]
-    lat.outer = water_universe
-    lat.orientation = 'y'
+    #openmc.deplete.CECMIntegrator(op, timesteps, power, timestep_units='d').integrate()
+    #results = openmc.deplete.Results("depletion_results.h5")
+    #time, keff = results.get_keff()
 
-    firts_ring = [tvel3_universe] * 60  # 60
-    second_ring = [tvel3_universe] + [tvel1_universe] * 8  # 54
-    second_ring *= 6
-    if tvegs_5inner_ring:
-     third_ring = [tvel1_universe] * 48
-    else:
-     third_ring = [tvel2_universe] + [tvel1_universe] * 7  # 48
-     third_ring *= 6
-    four_ring = [tvel1_universe] * 42  # 42
-    fife_ring = [tvel1_universe] * 3 + [guide_universe] + [tvel1_universe] * 5 + [guide_universe] + [
-    tvel1_universe] * 5 + [guide_universe] + [tvel1_universe] * 5 + [guide_universe] + [tvel1_universe] * 5 + [
-                  guide_universe] + [tvel1_universe] * 5 + [guide_universe] + [tvel1_universe] * 2  # 36
-    six_ring = [guide_universe] + [tvel1_universe] * 4  # 30
-    six_ring *= 6
-    if tvegs_5inner_ring:
-     seven_ring = [tvel1_universe] * 3 + [tvel2_universe]
-     seven_ring *= 6
-    else:
-     seven_ring = [tvel1_universe] * 3 + [tvel2_universe] + [tvel1_universe] * 4
-     seven_ring *= 3
-    eight_ring = [tvel1_universe] + [guide_universe] + [tvel1_universe]  # 18
-    eight_ring *= 6
-    nint_ring = [tvel1_universe] * 12  # 12
-    ten_ring = [tvel1_universe] * 6
-    inner_ring = [central_universe]
-    lat.universes = [firts_ring, second_ring, third_ring, four_ring, fife_ring, six_ring, seven_ring, eight_ring, nint_ring, ten_ring, inner_ring]
-    outer_surf=openmc.model.HexagonalPrism(edge_length=11*lat.pitch[0], orientation='y', boundary_type='reflective')
-    TVS_cell = openmc.Cell(fill=lat, region=-outer_surf & +bottom_surf & -top_surf)
-    TVS_universe=openmc.Universe(cells=[TVS_cell])
-    if verbose:
-        print(lat)
-    return TVS_universe, list(set((UO2_mat1, UO2_mat2, UO2_mat3))) + [guide_mat]
+    #settings.run_mode = 'fixed source'
+    #cecm = openmc.deplete.CECMIntegrator(operator, dt, power)
+    #cecm.integrate()
+    #source.strength = 18e0
+
+
+
+
+    colors = {water_mat: (32, 178, 170), water_mat1: (124, 252, 0), basket_mat: (1, 1, 1)}
+    color_data = dict(color_by='material', colors=colors)
+    width = np.array([24.6 *16, 24.6 * 16, ])
+    scale = 1
+    fig, ax = plt.subplots(2, 2)
+
+    geometry.root_universe.plot(width=width / scale, pixels=(100, 100), basis='xz', **color_data,
+                  origin=(0, 0, GeometryParams.tvel_heigh / 2 - 1), axes=ax[0][0])
+    geometry.root_universe.plot(width=width / scale, pixels=(100, 100), basis='xz', **color_data, origin=(0, 0, 0), axes=ax[1][1])
+    geometry.root_universe.plot(width=width / scale, pixels=(100, 100), basis='xz', **color_data,
+                  origin=(0, 0, -GeometryParams.tvel_heigh / 2 + 1),
+                  axes=ax[0][1])
+    geometry.root_universe.plot(width=width / scale, pixels=(10000, 10000), basis='xy', **color_data, origin=(0, 0, 0), axes=ax[1][0])
+
+    # ...and by openmc.Plots
+    plots = [openmc.Plot(), openmc.Plot(), openmc.Plot(), openmc.Plot(), ]
+    for i in range(4):
+        plots[i].width = width
+        plots[i].pixels = (10000, 10000)
+        plots[i].basis = 'xz'
+        plots[i].color_by = 'material'
+        plots[i].colors = colors
+    plots[0].origin = (0, 0, GeometryParams.tvel_heigh / 2 - 1)
+    plots[2].origin = (0, 0, -GeometryParams.tvel_heigh / 2 - 1)
+    plots[-1].basis = 'xy'
+
+    plots = openmc.Plots(plots)
+
+
+
+    #track=openmc.Track[('particle_2_50937.h5')]
+    #print(track)
+    plots.export_to_xml()
+    settings.export_to_xml()
+    geometry.export_to_xml()
+    #model.export_to_xml('model.xml')
+    openmc.plot_geometry()
+    openmc.run()
 
